38d421acfd3b8ffb9e3d6603c1fdded51091897d
Simon-Bertram <simonbertram@hotmail.com>
2025-01-07 15:13:09 +0000

    Add a GET route that returns request.user from the UserSchema

--- src/cfehome/api.py
+++ src/cfehome/api.py
@@ -1,8 +1,17 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
 
 api = NinjaAPI()
+
+class UserSchema(Schema):
+    username: str
+    is_authenticated: bool
+    email: str = None
 
 @api.get("/hello")
 def hello(request):
     print(request)
     return "Hello World"
+
+@api.get("/me", response=UserSchema)
+def me(request):
+    return request.user
