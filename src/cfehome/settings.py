1c6cc9f6fd40e74e7c2110f2fc52ec582e33e2eb
Simon-Bertram <simonbertram@hotmail.com>
2025-01-09 14:22:34 +0000

    Configure cors headers and allow the Nextjs origin

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third party
+    'corsheaders',
     'ninja_extra',
     'ninja_jwt',
     # internal
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -54,6 +56,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'cfehome.urls'
+
+CORS_URLS_REGEX = r"^/api/.*$"
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", # nextjs
+    "http://127.0.0.1:3000", # nextjs
+]
 
 TEMPLATES = [
     {
